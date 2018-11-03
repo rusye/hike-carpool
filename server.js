99a29852812b2617d1c466dfb28b76e3df58c77f
Russ yev <ruslanyev@gmail.com>
2018-11-02 17:38:35 -0700

    added that simple test it required

--- server.js
+++ server.js
@@ -1,4 +1,46 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+
+app.get('/', (req, res) => {
+  res.render('index.html')
+})
+
+
+// function runServer() {
+//   const port = process.env.PORT || 8080;
+//   return new Promise((resolve, reject) => {
+//       server = app
+//       .listen(port, () => {
+//           console.log(`Your app is listening on port ${port}`);
+//           resolve(server);
+//       })
+//       .on("error", err => {
+//           reject(err);
+//       });
+//   });
+// }
+
+// function closeServer() {
+//   return new Promise((resolve, reject) => {
+//       console.log("Closing server");
+//       server.close(err => {
+//           if (err) {
+//               reject(err);
+//               return;
+//           }
+//           resolve();
+//       });
+//   });
+// }
+
+// if (require.main === module) {
+//   runServer().catch(err => console.error(err));
+// }
+
+// module.exports = {app, runServer, closeServer};
+
+module.exports = {app};
