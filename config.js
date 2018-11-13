419e9af75d94d0973d18af2219aec2db4981bf34
Russ yev <ruslanyev@gmail.com>
2018-11-12 17:18:06 -0800

    added a config file

---
+++ config.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.DATABASE_URL =
+  process.env.DATABASE_URL || "mongodb://localhost/hiking-app";
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-hiking-app";
+exports.PORT = process.env.PORT || 8080;
