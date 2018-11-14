9ac9c1072da0fcb480afc59a7541917312bfc5bb
Russ yev <ruslanyev@gmail.com>
2018-11-13 17:57:55 -0800

    added the missing url in argument in runServer

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -23,23 +25,19 @@ let server;
 
 function runServer(databaseUrl, port = PORT) {
 	return new Promise((resolve, reject) => {
-		mongoose.connect(
-			databaseUrl,
-			err => {
-				if (err) {
-					return reject(err);
-				}
-				server = app
-					.listen(port, () => {
-						console.log(`Your app is listening on port ${port}`);
-						resolve();
-					})
-					.on("error", err => {
-						mongoose.disconnect();
-						reject(err);
-					});
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
 			}
-		);
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+				.on("error", err => {
+					mongoose.disconnect();
+					reject(err);
+				});
+			});
 	});
 }
 
@@ -56,10 +54,10 @@ function closeServer() {
 			});
 		});
 	});
-};
+}
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = { app, runServer, closeServer };
