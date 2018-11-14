0891023d84f8822511b976ebe8f9a386e2b96b6f
Russ yev <ruslanyev@gmail.com>
2018-11-13 17:09:19 -0800

    finished postRouter

--- postRouter.js
+++ postRouter.js
@@ -62,4 +62,66 @@ router.post('/', (req, res) => {
 
   User
     .findById(req.body.user_id)
+    .then(user => {
+      if (user) {
+        Posts
+          .create({
+            user: ObjectID(req.body.user_id),
+            hikename: req.body.hikename,
+            openseats: req.body.openseats,
+            content: req.body.content,
+            date: req.body.date
+          })
+          .then(post => res.status(201).json({
+            id: post.id,
+            user: user.username,
+            hikename: post.hikename,
+            openseats: post.openseats,
+            content: post.content,
+            date: post.date
+          }))
+          .catch(err => {
+            console.error(err);
+            res.status(500).json({message: 'Something went wrong'});
+          });
+      }
+      else {
+        const message = 'User not found';
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Something went wrong'});
+    });
 });
+
+// PUT request for posts
+router.put('/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({message: `ID's do not match`});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['hikename', 'openseats', 'content'];
+  updateableFields.forEach(field => {
+    if (info in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Posts
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Something went wrong'}));
+});
+
+router.delete('/:id', (req, res) => {
+  Posts
+    .findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({message: "Internal server error"}));
+});
+
+module.exports = router;
