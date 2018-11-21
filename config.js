ba22093c8251897831ad7587aafebaa13bbc72b6
Russ yev <ruslanyev@gmail.com>
2018-11-20 19:46:35 -0800

    added test to config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-hiking-app";
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'test';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
