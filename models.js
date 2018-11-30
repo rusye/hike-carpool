a13034b6dc98905841a68406cd5de9abb8604f06
Russ yev <ruslanyev@gmail.com>
2018-11-29 20:51:17 -0800

    revised date format

--- models.js
+++ models.js
@@ -5,12 +5,18 @@ mongoose.Promise = global.Promise;
 
 const {User} = require('./users/models');
 
+function local() {
+  let d = new Date(Date(2012, 11, 20, 3, 0, 0));
+  let local = d.toLocaleString('en-US');
+  return local;
+};
+
 const postSchema = mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   hikename: {type: String, required: true},
   openseats: {type: Number, required: true},
   content: {type: String, required: true},
-  date: {type: Date, default: Date.now()}
+  date: {type: String, default: local}
 });
 
 postSchema.pre('find', function(next) {
